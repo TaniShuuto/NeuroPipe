372d49c581c6e711a1275875fc9312101c74e510
TaniShuuto <278728922+TaniShuuto@users.noreply.github.com>
2026-08-13 03:22:02 +0900

    feat:MayaAdapter.connect()の例外処理実装

--- adapters/maya.py
+++ adapters/maya.py
@@ -1,8 +1,23 @@
+import socket
+
 from adapters.base import DCCAdapter
 from typing import Any
+
+from core.exceptions import DCCConnectionError
+
+
 class MayaAdapter(DCCAdapter):
+    def __init__(self,host:str,port:int,timeout:int):
+        self.host = host
+        self.port = port
+        self.timeout = timeout
     def connect(self):
-        pass
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.settimeout(self.timeout)
+        try:
+            sock.connect((self.host,self.port))
+        except OSError as e:
+            raise DCCConnectionError("Mayaとの接続に失敗しました。")
     def execute(self,code:str)->dict[str,Any]:
         pass
     def get_scene_info(self):
