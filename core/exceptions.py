ee95989db529d828eeca2e146312acdf696cab52
TaniShuuto <278728922+TaniShuuto@users.noreply.github.com>
2026-09-13 23:46:12 +0900

    feat:Exceptions.pyにAI側のExceptionを追加

--- core/exceptions.py
+++ core/exceptions.py
@@ -5,3 +5,9 @@ class DCCConnectionError(DCCAdapterError):
     pass
 class DCCExecutionError(DCCAdapterError):
     pass
+class AIError(Exception):
+    pass
+class AIConnectionError(AIError):
+    pass
+class AIGenerateError(AIError):
+    pass
