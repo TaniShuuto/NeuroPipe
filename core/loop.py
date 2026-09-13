d7ebee8592a20a3a49fb9e57ed879dcc0009b570
TaniShuuto <278728922+TaniShuuto@users.noreply.github.com>
2026-09-14 02:02:43 +0900

    feat:generate_code()のへ渡す引数の調整.DCCTypeの実装

--- core/loop.py
+++ core/loop.py
@@ -1,22 +1,25 @@
 from adapters.base import DCCAdapter
 from ai.base import AIBackend
-from core.config import load_config
-from adapters.maya import MayaAdapter
-from core.exceptions import DCCExecutionError
+from core.exceptions import DCCExecutionError, ToolExecutionError
 
-def loop(prompt:str,dcc:DCCAdapter,ai:AIBackend,language:str,loopcount:int) -> dict:
+
+def loop(prompt:str, dcc:DCCAdapter, ai:AIBackend, language:str, loopcount:int) -> dict:
+    if loopcount <= 0:
+        raise ToolExecutionError("ループ回数は0以上である必要があります")
     context = ""
     status = ""
+    dcc_type = dcc.get_dcc_type()
     for i in range(loopcount):
-        code = ai.generate_code(prompt + context,language)
+        code = ai.generate_code(language, dcc_type, prompt + context,)
         dcc.connect()
         try:
             dcc.execute(code)
             status = "ok"
             break
-        except DCCExecutionError as e: #as e が必要か分からないです。
+        except DCCExecutionError as e:
             context = prompt + code + str(e)
             status = "error"
             continue
 
     return {"status":status,"code":f"{code}"}
+
